8c24a3f2cdaf8eeb8e33baa9fb15874eed26e5f3
Brian Ngetich <kipronobrian97@gmail.com>
2024-04-19 19:45:49 +0300

    Big O

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+ for(let i = 0;i<array.length; i++){
+  const num = target - array[i]
+    for(let j=i+1;j<array.length;j++){
+      if((array[j] === num)){
+          return true
+      }
+    }
+ }
+ return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Iterate through our array
+
 */
 
 /*
